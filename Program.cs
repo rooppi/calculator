0ec8a50d31d02d42f7d1f004659cd66cb03d3817
rooppi <prostoxer@icloud.com>
2023-11-28 12:50:00 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите первое число");
+
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите оператор + - / *");
+
+string action = Console.ReadLine();
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine(number1 + number2);
+        break;
+
+    case "-":
+        Console.WriteLine(number1 - number2);
+        break;
+
+    case "/":
+        Console.WriteLine(number1 * number2);
+        break;
+    case "*":
+        Console.WriteLine(number1 / number2);
+        break;
+
+    default:
+        Console.WriteLine("");
+
+        break;
+
+}
+
+
+
+Console.ReadKey();
